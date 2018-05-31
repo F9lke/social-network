47d4ae056d1f40f6fcd98d1e29078ed74270aaad
Florian Götzrath <F.Goetzrath@gmx.de>
2018-05-31 11:17:02 +0200

    created Spinner and PrivateRoute components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
@@ -11,6 +11,8 @@ import store from './store';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
@@ -54,7 +56,9 @@ class App extends Component {
             <div className="container">
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
             </div>
 
             <Footer />
