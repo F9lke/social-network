94ffa264a13d4b8b7a4e2089294850a835d508d5
Florian Götzrath <F.Goetzrath@gmx.de>
2018-06-01 12:46:06 +0200

    implemented edit-profile functionality

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import Register from './components/auth/Register';
 
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
 
 import './App.css';
 
@@ -63,6 +64,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/edit-profile" component={EditProfile} />
               </Switch>
             </div>
 
