70c5029e8ab0f6252758080a44cece4903f840c6
Florian Götzrath <F.Goetzrath@gmx.de>
2018-06-03 20:17:27 +0200

    started to work on posts and create post

--- client/src/App.js
+++ client/src/App.js
@@ -8,26 +8,21 @@ import { clearCurrentProfile } from './actions/profileActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import PrivateRoute from './components/common/PrivateRoute';
+import NotFound from './components/not-found/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-
-import PrivateRoute from './components/common/PrivateRoute';
-
-import Dashboard from './components/dashboard/Dashboard';
-
-import CreateProfile from './components/create-profile/CreateProfile';
-import EditProfile from './components/edit-profile/EditProfile';
-
-import AddExperience from './components/add-credentials/AddExperience';
-import AddEducation from './components/add-credentials/AddEducation';
-
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
-
-import NotFound from './components/not-found/NotFound';
+import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
+import Dashboard from './components/dashboard/Dashboard';
+import Posts from './components/posts/Posts';
 
 import './App.css';
 
@@ -86,6 +81,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/add-education" component={AddEducation} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
 
               <Route exact path="/not-found" component={NotFound} />
